a1a1fb9fa8755adbb19b3e1507c535a11731ebda
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 01:56:57 +0300

    Added change

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import {store} from './store';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <Provider store={store}>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </Provider>
 );
 
