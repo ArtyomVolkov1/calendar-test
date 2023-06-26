9ad33c598b4d4c9aa411349f9d34c98b719a923c
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 01:56:46 +0300

    Added change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
+import AppRouter from './components/AppRouter';
+import Navbar from './components/Navbar';
+import { Layout } from 'antd';
 
+const { Content } = Layout;
 const App:FC = () => {
   return (
-    <div>
-      Работает!
-    </div>
+    <Layout>
+      <Navbar />
+      <Content>
+      <AppRouter />
+      </Content>
+    </Layout>
   )
 }
 export default App;
