6ebb9b962d7332bb2edf76b905a75883d1b69346
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 13:11:01 +0300

    Added changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
 import { Layout } from 'antd';
-
+import './App.css';
 const { Content } = Layout;
 const App:FC = () => {
   return (
