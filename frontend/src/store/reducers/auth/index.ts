27d2f9852c352f3f70745b1440eac28f87b44126
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 13:13:09 +0300

    Added changes

---
+++ frontend/src/store/reducers/auth/index.ts
@@ -0,0 +1,13 @@
+import { AuthState, AuthAction, AuthActionEnum } from './types';
+const initialState: AuthState = {
+    isAuth: false
+}
+
+export default function authReducer(state = initialState, action: AuthAction): AuthState {
+    switch (action.type) {
+        case AuthActionEnum.SET_AUTH: 
+            return {...state, isAuth: action.payload}
+        default: 
+            return state;
+    }
+}
---
+++ frontend/src/store/reducers/auth/types.ts
@@ -0,0 +1,16 @@
+
+export interface AuthState {
+    isAuth: boolean;
+}
+
+export enum AuthActionEnum {
+    SET_AUTH = 'SET_AUTH'
+}
+
+export interface SetAuthAction {
+    type: AuthActionEnum.SET_AUTH ;
+    payload: boolean;
+}
+
+export type AuthAction = 
+    SetAuthAction
