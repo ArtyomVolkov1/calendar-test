fe8ca2b2fbd0452de0ee37830a892340c6fece04
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 13:11:40 +0300

    Added changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,14 @@
 import React, {FC} from 'react'
+import {Layout, Row} from 'antd';
+import LoginForm from '../components/LoginForm';
 
 const Login: FC = () => {
   return (
-    <div>Login</div>
+    <Layout>
+      <Row justify='center' align='middle' className='h100'>
+        <LoginForm />
+      </Row>
+    </Layout>
   )
 }
 
