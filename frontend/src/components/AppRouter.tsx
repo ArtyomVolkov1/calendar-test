c97264aecf9164bd204eaf1678fe55fb30ccf482
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 13:11:13 +0300

    Added changes

--- frontend/src/components/AppRouter.tsx
+++ frontend/src/components/AppRouter.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Routes, Route,  Navigate } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from '../router';
+import {useTypedSelector} from '../hooks/usedTypedSelector';
 const AppRouter = () => {
-    const auth: boolean = true;
+   const {isAuth} = useTypedSelector(state => state.auth)
   return (
-    auth ?
+    isAuth ?
     <Routes>
        {privateRoutes.map((route) => 
        <Route key={route.path}
         path={route.path}
-        element={route.element}/>)}
+        element={route.element }/>)}
         <Route path={'*'} element={<Navigate to="/" replace />} />
     </Routes>
     :
