7b09b4d8d3bea8e2912b725f80248d79c373d507
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-27 13:11:23 +0300

    Added changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,41 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { Layout, Row, Menu } from 'antd';
-
+import { useLocation, useNavigate, useNavigation } from 'react-router-dom';
+import { RouteNames } from '../router';
+import {useTypedSelector} from '../hooks/usedTypedSelector';
 const { Header } = Layout;
 
 const Navbar: FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log('Current location is ', location);
+  }, [location]);
+  const {isAuth} = useTypedSelector(state => state.auth)
   return (
     <Header >
         <Row justify='end'>
-            <Menu theme="dark" mode="horizontal" selectable={false}>
-                <Menu.Item key={1}>Логин</Menu.Item>
+            {isAuth 
+                ? 
+                <>
+                <div style={{color: 'white'}}>Artyom</div>
+                <Menu theme="dark" mode="horizontal" selectable={false}>
+                <Menu.Item
+                 onClick={() => console.log('Выйти')}
+                   key={1}>
+                    Выйти
+                </Menu.Item>
             </Menu>
+            </>
+                : 
+                <Menu theme="dark" mode="horizontal" selectable={false}>
+                <Menu.Item
+                 onClick={() => navigate(RouteNames.LOGIN)}
+                   key={1}>
+                    Логин
+                </Menu.Item>
+            </Menu>
+          }
         </Row>
     </Header>
   )
