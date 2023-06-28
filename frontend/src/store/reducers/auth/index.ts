a492f7768948a76b62d00f4d497ee469a3975729
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-29 02:26:32 +0300

    Added change

--- frontend/src/store/reducers/auth/index.ts
+++ frontend/src/store/reducers/auth/index.ts
@@ -1,13 +1,25 @@
+import { act } from 'react-dom/test-utils';
+import { IUser } from '../../../models/IUser';
 import { AuthState, AuthAction, AuthActionEnum } from './types';
+
 const initialState: AuthState = {
-    isAuth: false
-}
+    isAuth: false,
+    user: {} as IUser,
+    isLoading: false,
+    error: ''
+};
 
 export default function authReducer(state = initialState, action: AuthAction): AuthState {
     switch (action.type) {
         case AuthActionEnum.SET_AUTH: 
-            return {...state, isAuth: action.payload}
+            return {...state, isAuth: action.payload, isLoading: false}
+        case AuthActionEnum.SET_USER:
+            return {...state, user: action.payload}
+        case AuthActionEnum.SET_IS_LOADING:
+            return {...state, isLoading: action.payload}
+        case AuthActionEnum.SET_ERROR:
+            return {...state, error: action.payload, isLoading: false}
         default: 
             return state;
-    }
-}
+    };
+};
