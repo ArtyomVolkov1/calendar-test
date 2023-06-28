b0fc367500774b4d7726cb452d0dd4bd7a92cd5b
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-29 02:26:17 +0300

    Added change

--- frontend/src/pages/Event.tsx
+++ frontend/src/pages/Event.tsx
@@ -1,8 +1,42 @@
-import React, { FC } from 'react'
+import { Button, Layout, Modal, Row } from 'antd'
+import React, { Dispatch, FC, useEffect, useState } from 'react'
+import CalendarEvent from '../components/CalendarEvent'
+import EventForm from '../components/EventForm';
+import { useDispatch } from 'react-redux';
+import {EventActionCreators} from '../store/reducers/event/actions-creator';
+import { useTypedSelector } from '../hooks/usedTypedSelector';
+import { IEvent } from '../models/IEvent';
 
 const Event: FC = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const {guests, events} = useTypedSelector(state => state.event)
+  const dispatch: Dispatch<any> = useDispatch();
+  const {user} = useTypedSelector(state => state.auth);
+  useEffect(() => {
+    dispatch(EventActionCreators.fetchGuests());
+    dispatch(EventActionCreators.fetchEvents(user.username));
+  },[]);
+
+  const addNewEvent = (event: IEvent) => {
+    setModalVisible(false);
+    dispatch(EventActionCreators.createEvent(event))
+  }
+
   return (
-    <div>Event Page</div>
+    <Layout>
+      <CalendarEvent events={events}/>
+      <Row justify='center'>
+        <Button onClick={() => setModalVisible(true)}>Добавить событие</Button>
+      </Row>
+      <Modal
+      title='Добавить событие'
+      open={modalVisible}
+      footer={null}
+      onCancel={() => setModalVisible(false)}
+      >
+        <EventForm guests={guests} submit={addNewEvent} />
+      </Modal>
+    </Layout>
   )
 }
 
