be1e046f768e4825f97242076d33b08883ab6648
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-29 02:25:49 +0300

    Added change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
 import { Layout } from 'antd';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { AuthActionCreators } from './store/reducers/auth/action-creators';
+import { IUser } from './models/IUser';
 const { Content } = Layout;
+
 const App:FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if(localStorage.getItem('auth')) {
+      dispatch(AuthActionCreators.setUser({username: localStorage.getItem('username' || '' )} as IUser));
+      dispatch(AuthActionCreators.setIsAuth(true));
+    }
+  })
   return (
     <Layout>
       <Navbar />
