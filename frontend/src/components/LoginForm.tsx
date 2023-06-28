021b41af63e1bfaf93cfcc82a7175fb9f0293fb0
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-29 02:26:00 +0300

    Added change

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,19 +1,33 @@
-import React, { FC } from 'react'
+import React, { Dispatch, FC, useState } from 'react'
 import { Form, Input, Button } from 'antd';
 import { rules } from '../utils/rules';
+import { useDispatch } from 'react-redux';
+import { AuthActionCreators } from '../store/reducers/auth/action-creators';
+import { useTypedSelector } from '../hooks/usedTypedSelector';
 
 const LoginForm: FC = () => {
-  const submit = () => console.log('submit')
+  const dispatch: Dispatch<any> = useDispatch();
+  const { error, isLoading } = useTypedSelector(state => state.auth);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const submit = () => {
+    dispatch(AuthActionCreators.login(username, password));
+  }
   return (
     <Form
     onFinish={submit}
     >
+      {
+      error && <div style={{color: 'red'}}>
+        {error}
+      </div>
+      }
          <Form.Item
       label="Имя пользователя"
       name="username"
       rules={[rules.required('Пожалуйста введите имя')]}
     >
-      <Input />
+      <Input value={username} onChange={e => setUsername(e.target.value)}/>
     </Form.Item>
 
     <Form.Item
@@ -21,10 +35,10 @@ const LoginForm: FC = () => {
       name="password"
       rules={[rules.required('Пожалуйста введите пароль')]}
     >
-      <Input.Password />
+      <Input.Password value={password} onChange={e => setPassword(e.target.value)} />
     </Form.Item>
     <Form.Item wrapperCol={{ offset: 10, span: 20 }}>
-      <Button type="primary" htmlType="submit">
+      <Button type="primary" htmlType="submit" loading={isLoading}>
         Войти
       </Button>
     </Form.Item>
