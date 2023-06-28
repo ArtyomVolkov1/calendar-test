59b932c6cf4ac1adbf3de80e1a905f44c98bea4f
Artyom Volkov <artemvolk2099@yandex.ru>
2023-06-29 02:26:54 +0300

    Added change

--- frontend/src/store/reducers/auth/types.ts
+++ frontend/src/store/reducers/auth/types.ts
@@ -1,10 +1,17 @@
+import { IUser } from '../../../models/IUser';
 
 export interface AuthState {
     isAuth: boolean;
+    user: IUser;
+    isLoading: boolean;
+    error: string;
 }
 
 export enum AuthActionEnum {
-    SET_AUTH = 'SET_AUTH'
+    SET_AUTH = 'SET_AUTH',
+    SET_USER = 'SET_USER',
+    SET_IS_LOADING = 'SET_IS_LOADING',
+    SET_ERROR = 'SET_ERROR',
 }
 
 export interface SetAuthAction {
@@ -12,5 +19,23 @@ export interface SetAuthAction {
     payload: boolean;
 }
 
+export interface SetUserAction {
+    type: AuthActionEnum.SET_USER ;
+    payload: IUser;
+}
+
+export interface SetIsLoadingAction {
+    type: AuthActionEnum.SET_IS_LOADING ;
+    payload: boolean;
+}
+
+export interface SetErrorAction {
+    type: AuthActionEnum.SET_ERROR ;
+    payload: string;
+}
+
 export type AuthAction = 
-    SetAuthAction
+    SetAuthAction |
+    SetUserAction |
+    SetIsLoadingAction |
+    SetErrorAction
